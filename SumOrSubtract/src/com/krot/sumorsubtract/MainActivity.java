8a580642ee6d39e6a76aaf1d902a8800782395a3
Nikolai Krot <talpus@gmail.com>
2013-12-21 23:53:07 +0300

    saving/restoring state implemented. However, could not test restoration

--- SumOrSubtract/src/com/krot/sumorsubtract/MainActivity.java
+++ SumOrSubtract/src/com/krot/sumorsubtract/MainActivity.java
@@ -21,14 +21,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        Log.d("onCreate was triggered", "zzz");
+
+        // Log.d("onCreate was triggered", "zzz");
 
         if (savedInstanceState == null) {
             operationHistory = new OperationHistory();
         } else {
             // TODO: restore history from saved object
-            Log.d("Restore from saved", "mmm. how?");
+            Log.d("Restore from saved", "mmm. how?"); // TODO: never happens!
         }
     }
 
@@ -47,8 +47,9 @@ public class MainActivity extends Activity {
         String operationId = tvOperationId.getText().toString();
 
         if (operationId.length() == 0) {
-            // TODO: what should we do if nothing was input? perhaps it makes sense
-            // disabling the = button when the edit box is empty? Then this branch is useless
+            // TODO: what should we do if nothing was input? perhaps it makes
+            // sense disabling the = button when the edit box is empty? 
+            // Then this branch is useless
             // Log.d("Empty operation id", operationId);
         } else {
             // the inputType was set to numeric, that restricts input to non-negative numbers
@@ -68,7 +69,8 @@ public class MainActivity extends Activity {
     };
 
     protected void onActivityResult(int requestCode, int resultCode, Intent resultData) {
-        if (requestCode == QUERY_OPERANDS_REQUEST_FOR_SUM || requestCode == QUERY_OPERANDS_REQUEST_FOR_SUBTRACT) {
+        if (requestCode == QUERY_OPERANDS_REQUEST_FOR_SUM
+                || requestCode == QUERY_OPERANDS_REQUEST_FOR_SUBTRACT) {
 
             TextView tv_op1 = (TextView) findViewById(R.id.operand_1);
             TextView tv_op2 = (TextView) findViewById(R.id.operand_2);
@@ -93,7 +95,7 @@ public class MainActivity extends Activity {
                 tv_op2.setText(op2);
                 tv_res.setText(String.valueOf(res));
 
-            } else if (resultCode == RESULT_CANCELED ) {
+            } else if (resultCode == RESULT_CANCELED) {
                 // reset all text view to defaults
                 tv_op1.setText(R.string.the_1st_operand);
                 tv_op2.setText(R.string.the_2nd_operand);
@@ -106,23 +108,23 @@ public class MainActivity extends Activity {
     protected void showHistoryItem() {
         if (operationHistory.wasMostRecentQueryInvalid()) {
             showDialog(INVALID_HISTORY_ITEM_DLG);
-    
+
             // clear invalid user query
             TextView viewOperationId = (TextView) findViewById(R.id.ev_history_operation_id);
             viewOperationId.setText("");
         }
-        
+
         if (operationHistory.hadValidQueries()) {
-            TextView viewOperationId     = (TextView) findViewById(R.id.ev_history_operation_id);
-            TextView viewOperationName   = (TextView) findViewById(R.id.tv_history_operation_name);
+            TextView viewOperationId = (TextView) findViewById(R.id.ev_history_operation_id);
+            TextView viewOperationName = (TextView) findViewById(R.id.tv_history_operation_name);
             TextView viewOperationResult = (TextView) findViewById(R.id.tv_history_result_value);
-        
+
             viewOperationId.setText(String.valueOf(operationHistory.getHistoryItemId()));
             viewOperationName.setText(operationHistory.getOperation());
             viewOperationResult.setText(String.valueOf(operationHistory.getResult()));
         }
     }
-    
+
     protected void addToHistory(String opName, int opResult) {
         operationHistory.addItem(opName, opResult);
         TextView viewHistorySize = (TextView) findViewById(R.id.history_size_value);
@@ -132,27 +134,52 @@ public class MainActivity extends Activity {
     @Override
     protected Dialog onCreateDialog(int dialogId) {
         // http://www.vogella.com/articles/AndroidDialogs/article.html#tutorial_alertdialog
-        
+
         switch (dialogId) {
         case INVALID_HISTORY_ITEM_DLG:
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            
-            String msg = getString(R.string.invalid_history_item_id)
-                         .concat(": ")
-                         .concat(String.valueOf(operationHistory.getInvalidItemId()));
-            
+
+            String msg = getString(R.string.invalid_history_item_id).concat(": ")
+                    .concat(String.valueOf(operationHistory.getInvalidItemId()));
+
             builder.setMessage(msg)
-                   .setPositiveButton(R.string.gotyou, new DialogInterface.OnClickListener() {
-                       @Override
-                       public void onClick(DialogInterface dialog, int which) {
-                           // TODO: what to write here?
-                       }
-                    });
-            
+                    .setPositiveButton(R.string.gotyou,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // TODO: what to write here?
+                                }
+                            });
+
             Dialog dialog = builder.create();
             dialog.show();
         }
-        
+
         return super.onCreateDialog(dialogId);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d("onSaveInstanceState", " was called");
+        outState.putParcelable("operationHistory", operationHistory);
+    }
+
+    // TODO: this event is never triggered
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d("onRestoreInstanceState", " was called");
+        operationHistory = savedInstanceState.getParcelable("operationHistory");
+    }
+
+    /*
+     // TODO: this is even is triggered but have no idea of how to get Bundle in there. 
+     //       and this seems to be a wrong approach
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("onResume", " was called");
+        operationHistory = getIntent().getParcelableExtra("operationHistory"); // CRASH
+    }*/
 }
