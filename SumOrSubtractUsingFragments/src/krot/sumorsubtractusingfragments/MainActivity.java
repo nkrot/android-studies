1553f6475c4d6e799bb80d2ff67f059b3c1737e8
Nikolai Krot <talpus@gmail.com>
2013-12-29 01:17:43 +0300

    refactored

--- SumOrSubtractUsingFragments/src/krot/sumorsubtractusingfragments/MainActivity.java
+++ SumOrSubtractUsingFragments/src/krot/sumorsubtractusingfragments/MainActivity.java
@@ -3,7 +3,6 @@ package krot.sumorsubtractusingfragments;
 import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends Activity
         implements OperandsFragment.OnComputeOrCancelPressedListener,
@@ -12,36 +11,19 @@ public class MainActivity extends Activity
     private OperationsFragment operationsFragment;
     private OperandsFragment operandsFragment;
     private int currentOperation;
+    private int operationsContainer; // for placing operationsFragment in it
+    private int operandsContainer; // for placing operandsFragment in it
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
-        //       if (findViewById(R.id.main_container) != null) {
-        //           if (savedInstanceState != null) {
-        //               return;
-        //           }
-
         if (isTablet()) {
-            Log.d("onCreate()", " in Tablet mode");
-
-            operationsFragment = new OperationsFragment();
-            operandsFragment = new OperandsFragment();
-            FragmentTransaction ft = getFragmentManager().beginTransaction();
-            ft.add(R.id.operations_container, operationsFragment);
-            ft.add(R.id.operands_container, operandsFragment);
-            ft.commit();
-
+            initTablet();
         } else {
-            Log.d("onCreate()", " in Handset mode");
-            operationsFragment = new OperationsFragment();
-            getFragmentManager().beginTransaction()
-                    .add(R.id.main_container, operationsFragment)
-                    .commit();
+            initHandset();
         }
-
-        //       }
     }
 
     public void onSetOperation(int operation) {
@@ -79,22 +61,14 @@ public class MainActivity extends Activity
 
     public void showOperandsFragment() {
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        if (isTablet()) {
-            transaction.replace(R.id.operands_container, operandsFragment);
-        } else {
-            transaction.replace(R.id.main_container, operandsFragment); // using .add here leads to two fragments being shown at once!
-            // transaction.addToBackStack(null); // TODO: not necessary as I'm never using popBackStack()
-        }
+        transaction.replace(operandsContainer, operandsFragment); // using .add here leads to two fragments being shown at once!
+        // transaction.addToBackStack(null); // TODO: not necessary as I'm never using popBackStack()
         transaction.commit();
     }
 
     public void showOperationsFragment() {
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        if (isTablet()) {
-            transaction.replace(R.id.operations_container, operationsFragment);
-        } else {
-            transaction.replace(R.id.main_container, operationsFragment);
-        }
+        transaction.replace(operationsContainer, operationsFragment);
         // transaction.addToBackStack(null); // TODO: not necessary as I'm never using popBackStack()
         transaction.commit();
     }
@@ -103,4 +77,25 @@ public class MainActivity extends Activity
         return getResources().getBoolean(R.bool.has_two_panes);
     }
 
+    private void initTablet() {
+        operationsContainer = R.id.operations_container;
+        operandsContainer = R.id.operands_container;
+
+        operationsFragment = new OperationsFragment();
+        operandsFragment = new OperandsFragment();
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.add(operationsContainer, operationsFragment);
+        ft.add(operandsContainer, operandsFragment);
+        ft.commit();
+    }
+
+    private void initHandset() {
+        operationsContainer = operandsContainer = R.id.main_container;
+        operationsFragment = new OperationsFragment();
+
+        getFragmentManager().beginTransaction()
+                .add(operationsContainer, operationsFragment)
+                .commit();
+    }
 }
