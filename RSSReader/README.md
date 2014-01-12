8b676f94dec038d32b5229a7a4cc175f78b4a337
Nikolai Krot <talpus@gmail.com>
2014-01-12 16:39:24 +0300

    progress dialog added

--- RSSReader/README.md
+++ RSSReader/README.md
@@ -10,11 +10,13 @@ TODO
 3. align the picture to the top
 4. ? show the complete News text (description) in a separate activity. In this case the main list may contain the Image, the Date and the Title only
 5. DONE. perform RSS downloading and parsing in background (using AsyncTask)
-6. Add progress bar.
+6. DONE. Show RSS download progress, using ProgressDialog
+7. ? How to use ProgressBar instead of ProgressDialog?
 
 Useful links
 ============
 
 1. RSS format: http://www.realcoding.net/article/view/1883
 2. Example code: http://code.google.com/p/android-imagedownloader/
+3. http://www.android-ios-tutorials.com/182/show-progressbar-while-downloading-image-using-asynctask-in-android/
 
--- RSSReader/src/krot/rssreader/RSSDownloaderTask.java
+++ RSSReader/src/krot/rssreader/RSSDownloaderTask.java
@@ -3,27 +3,41 @@ package krot.rssreader;
 import nasa.rss.pictureoftheday.RSSFeed;
 import nasa.rss.pictureoftheday.RSSFeedDownloader;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ListView;
 
-public class RSSDownloaderTask extends AsyncTask<String, Void, RSSFeed> {
+public class RSSDownloaderTask extends AsyncTask<String /*param*/, Void /*progress*/, RSSFeed /*result*/> {
     private final Activity targetActivity;
     private final ListView targetView;
+    private ProgressDialog progressDialog;
 
     public RSSDownloaderTask(Activity activity, ListView view) {
         // TODO: perhaps using WeakReference would be preferable?
         targetActivity = activity;
         targetView = view;
+        progressDialog = null;
     }
 
     @Override
     public RSSFeed doInBackground(String... args) {
+        //publishProgress(true); // with ProgressBar
         return downloadRSSFeed();
     }
 
     @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressDialog = ProgressDialog.show(targetView.getContext(), null,
+                targetView.getResources().getString(R.string.downloading));
+    }
+
+    @Override
     protected void onPostExecute(RSSFeed feed) {
+        //publishProgress(false); // with ProgressBar
+        progressDialog.dismiss(); // with ProgressDialog
+
         if (isCancelled()) {
             feed = null;
         }
@@ -33,6 +47,15 @@ public class RSSDownloaderTask extends AsyncTask<String, Void, RSSFeed> {
             targetView.setAdapter(adapter);
         }
     }
+
+    /* 
+     * with ProgressBar. unfinished
+    @Override
+    protected void onProgressUpdate(Boolean... progress) {
+        Log.d("PROGRESSBAR", "updating");
+        targetActivity.setProgressBarIndeterminateVisibility(progress[0]);
+    }
+    */
 
     private RSSFeed downloadRSSFeed() {
         RSSFeedDownloader downloader = new RSSFeedDownloader();
